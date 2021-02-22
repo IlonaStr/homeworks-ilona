f6d11ed635da656c87efde856ac0a363ce817f5f
IlonaStr <hellosidney17@gmail.com>
2021-02-23 01:23:34 +0200

    refactoring 2

--- 01-homework/index.js
+++ 01-homework/index.js
@@ -3,10 +3,10 @@ const tabletPrice = 123.965;
 const headphonesPrice = 90.2345;
 
 
-const max = Math.max(phonePrice, tabletPrice, headphonesPrice);
-const min = Math.min(phonePrice, tabletPrice, headphonesPrice);
-console.log(`Maximum: `, max );
-console.log(`Minimum: `, min);
+const maxPrice = Math.max(phonePrice, tabletPrice, headphonesPrice);
+const minPrice = Math.min(phonePrice, tabletPrice, headphonesPrice);
+console.log(`Maximum: `, maxPrice);
+console.log(`Minimum: `, minPrice);
 
 
 const allProductsPrice = phonePrice + tabletPrice + headphonesPrice;
@@ -24,18 +24,12 @@ const roundedTtl = Math.ceil(prodTtlEven / 100) * 100;
 console.log(`Total rounded to hundreds: `, roundedTtl);
 
 
-// if(prodTtlEven %2 === 0) {
-//     console.log(`true(even)`)
-// } else {
-//     console.log(`false(uneven)`)
-// }
-
 const pairedNumber = prodTtlEven % 2 === 0;
 console.log(pairedNumber);
 
 
 const clientPays = 500;
-const totalRest = allProductsPrice - clientPays;
+const totalRest = clientPays - allProductsPrice;
 console.log(`Rest after client pays 500 UAH: `, totalRest);
 
 
@@ -53,15 +47,15 @@ console.log(`Final cost with discount: `, costWithDiscount);
 
 //Advanced
 console.log (`
-Maximum price: ${Math.max(phonePrice, tabletPrice, headphonesPrice)}
-Minimum price: ${Math.min(phonePrice, tabletPrice, headphonesPrice)}
-Price of all products: ${allProductsPrice};
-Price of all products evened to a less: ${prodTtlEven};
-Price of all products evened to hundreds: ${roundedTtl};
-Boolean value: ${pairedNumber};
-Rest after client pays 500UAH: ${totalRest};
-Rest evened after client paid 500UAH: ${avrEvened};
-Random discount: ${discount}%;
-A discount the client gets: ${clientsDiscount};
-Final cost with discount: ${costWithDiscount};
+    Maximum price: ${maxPrice};
+    Minimum price: ${minPrice};
+    Price of all products: ${allProductsPrice};
+    Price of all products evened to a less: ${prodTtlEven};
+    Price of all products evened to hundreds: ${roundedTtl};
+    Boolean value: ${pairedNumber};
+    Rest after client pays 500UAH: ${totalRest};
+    Rest evened after client paid 500UAH: ${avrEvened};
+    Random discount: ${discount}%;
+    A discount the client gets: ${clientsDiscount};
+    Final cost with discount: ${costWithDiscount};
 `)
