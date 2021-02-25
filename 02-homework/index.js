6d04f2829f95d84f808e2ea5814f5c79fc86e871
IlonaStr <hellosidney17@gmail.com>
2021-02-25 23:21:18 +0200

    fixed hw 2

--- 02-homework/index.js
+++ 02-homework/index.js
@@ -1,28 +1,37 @@
-let initialNumber;
-let finalNumber;
+let initialNumber = null;
+let finalNumber = null;
 
 do {
     initialNumber = +prompt('Enter please the initial number');
-    finalNumber = +prompt('Enter please the final number');
-
-    if(!Number.isInteger(initialNumber) || !Number.isInteger(finalNumber)) {
+    
+    if(!Number.isInteger(initialNumber)) {
         alert('Please insert only an integer number');
     } else if (initialNumber >= finalNumber) {
-        alert(`The final number should be bigger, than initial`)
+        alert(`The final number should be bigger, than initial`);
+        continue;
     }
-} while (!Number.isInteger(initialNumber) || !Number.isInteger(finalNumber));
+} while (!Number.isInteger(initialNumber));
+
+do {
+    finalNumber = +prompt('Enter please the final number');
+
+    if(!Number.isInteger(finalNumber)) {
+        alert('Please insert only an integer number');
+    } else if (initialNumber >= finalNumber) {
+        alert(`The final number should be bigger, than initial`);
+        continue;
+    }
+} while (!Number.isInteger(finalNumber));
 
 
 let numbersSum = 0;
-let skipEvenNumbers = confirm('Would you like to skip even numbers?');
+const skipEvenNumbers = confirm('Would you like to skip even numbers?');
 
-    for(let i = initialNumber; i <= finalNumber; i++) {
-      if(skipEvenNumbers === true) {
-        if(i % 2 !== 0) {
-            numbersSum += i;
-        } 
-        } else {
-        numbersSum += i;
-    }
+for(let i = initialNumber; i <= finalNumber; i++) {
+ if(skipEvenNumbers && i % 2 === 0) {
+   continue;
+ }
+    numbersSum += i;
 }
+
 console.log(`The total sum is ${numbersSum}`)
