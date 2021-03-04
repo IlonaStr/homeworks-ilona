fbba1f15c319bba9779df96e4f4d303eb5b0240e
IlonaStr <hellosidney17@gmail.com>
2021-03-04 22:14:30 +0200

    second fixes

--- 03-homework/index.js
+++ 03-homework/index.js
@@ -2,13 +2,14 @@
 
 const getMaxDigit = function (number) {
     let maxDigit = 0;    
-    let numberIntoString = String(number);
+    const numberIntoString = String(number);
 
     for(let i = 0; i < numberIntoString.length; i++) {
         if(maxDigit < numberIntoString[i]) {
             maxDigit = numberIntoString[i];
         }
     }
+
     return maxDigit;
 }
 console.log(`${getMaxDigit(5497)}`)
@@ -19,15 +20,14 @@ const nameChange = function(name) {
     
     return name[0].toUpperCase() + name.slice(1).toLowerCase();
 }
+
 console.log(`${nameChange("draKe")}`)
 
 //task 4
 
-const salaryMinusTax = function(salary, tax) {
-    tax = 18 + 1.5;
-    return salary - ((salary * tax) / 100);
-}
-console.log(`${salaryMinusTax(1000, 19.5)}`)
+const salaryMinusTax = (salary, tax) => salary - ((salary * tax) / 100);
+
+console.log(`${salaryMinusTax(1000, 18 + 1.5)}`)
 
 //task 5
 
@@ -41,11 +41,16 @@ console.log(`${getRandomNumber(1, 10)}`);
 
 const countLetter = function(letter, word) {
     let count = null;
-    let wordToLower = word.toLowerCase(word);
+
+    const wordToLower = word.toLowerCase(word);
+
     for(let i = 0; i < wordToLower.length; i++) {
-        if(wordToLower[i] === letter)
-        count++;
+        if(wordToLower[i] === letter) {
+            count++;
+        }
+
     }
+
     return count;
 }
 
@@ -55,11 +60,13 @@ console.log(`${countLetter('a', 'Abraham')}`);
 
 const isPalindrome = function(palindrome) {
     let wordIsPalindrome = true;
+
     for (let i = 0, j = palindrome.length - 1; i < palindrome.length; i++, j--) {
         if (palindrome[i] !== palindrome[j])
         wordIsPalindrome = false;
         break;
     }
+
     return wordIsPalindrome;
 }
 
