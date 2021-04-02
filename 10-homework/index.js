48165f25916426f06aea56acdb47acca3d754924
IlonaStr <hellosidney17@gmail.com>
2021-04-02 13:42:14 +0300

    fixed appearance

--- 10-homework/index.js
+++ 10-homework/index.js
@@ -1,22 +1,24 @@
 function getRandomColor() {
-    let color = '';   
-    for (let i = 0; i < 6; i++) {
-      color += (Math.random() * 16 | 0).toString(16);
-    }
-    return '#' + color;
+  let color = "";
+
+  for (let i = 0; i < 6; i++) {
+    color += ((Math.random() * 16) | 0).toString(16);
   }
 
-document.getElementById("squaresButton").onclick = function generateBlocks() {
-const squares = document.getElementById("squares");
-squares.style.cssText = `height: 250px; width: 250px; display: flex; flex-wrap: wrap`;
-squares.setAttribute("id", "square_container");
-
-    for(let i = 1; i <= 25; i++) {
-        const square = document.createElement("div");
-        square.style.cssText = `height: 50px; width: 50px; display: inline-block`;
-        square.style.backgroundColor = getRandomColor();
-        squares.append(square);
-    }
+  return "#" + color;
 }
 
-console.log(generateBlocks())
+function generateBlocks() {
+  const squares = document.getElementById("squares");
+  squares.style.cssText = `height: 250px; width: 250px; display: flex; flex-wrap: wrap`;
+  squares.setAttribute("id", "square_container");
+
+  for (let i = 1; i <= 25; i++) {
+    const square = document.createElement("div");
+    square.style.cssText = `height: 50px; width: 50px; display: inline-block`;
+    square.style.backgroundColor = getRandomColor();
+    squares.append(square);
+  }
+}
+
+document.getElementById("squaresButton").onclick = generateBlocks();
