b21919a6b12861f2bfce1d204ba492db203be9e4
IlonaStr <hellosidney17@gmail.com>
2021-04-05 00:45:11 +0300

    hw 13

--- 13-homework/index.js
+++ 13-homework/index.js
@@ -12,34 +12,36 @@ function getPlanets() {
 function getActors() {
   axios.get(`https://swapi.dev/api/films/${2}/`).then((res) => {
     const actorsPage = res.data.characters;
-    list.innerHTML = `<h2>Actors<h2>`;
+    list.innerHTML = `<h2>Actors' list:<h2>`;
     for (let i = 0; i < actorsPage.length; i++) {
       axios.get(`https${actorsPage[i].substring(4)}`).then((result) => {
-        const actor = document.createElement("li");
-        actor.className = "actor";
-        actor.innerHTML = `
-                Name: ${result.data.name};<br>
-                Year of birth: ${result.data.birth_year};<br>
-                Gender: ${result.data.gender}<br>
-                `;
-        list.appendChild(actor);
+        const characterName = result.data.name;
+        const characterBirth = result.data.birth_year;
+        const characterGender = result.data.gender;
+        list.insertAdjacentHTML(
+          "beforeend",
+          `<h3>Name: ${characterName}</h3>
+            <h4>Birth date: ${characterBirth}</h4>
+            <h4 class="gender">Gender: ${characterGender}</h4>`
+        );
       });
     }
   });
 }
 
 function getPlanetsPage(num) {
-  list.innerHTML = `<h2>Planets</h2>`;
+  list.innerHTML = `<h2>Planets' list</h2>`;
   axios.get(`https://swapi.dev/api/planets/?page=${num}`).then((res) => {
     for (let i = 0; i < res.data.results.length; i++) {
-      const planet = document.createElement("li");
-      planet.className = "planet";
-      planet.innerHTML = `
-            Name: ${res.data.results[i].name};
-            Climate: ${res.data.results[i].climate};
-            Diameter: ${res.data.results[i].diameter};
-            `;
-      list.appendChild(planet);
+      const planetName = res.data.results[i].name;
+      const planetClimate = res.data.results[i].climate;
+      const planetSize = res.data.results[i].diameter;
+      list.insertAdjacentHTML(
+        "beforeend",
+        `<h3>Name: ${planetName}</h3>
+            <h4>Climate: ${planetClimate}</h4>
+            <h4 class="size">Size: ${planetSize}</h4>`
+      );
     }
     const next = document.createElement("input");
     const previous = document.createElement("input");
