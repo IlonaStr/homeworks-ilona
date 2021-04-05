a1cdc0e4bf2f9802b252d8141843914ae533ade3
IlonaStr <hellosidney17@gmail.com>
2021-04-06 00:18:52 +0300

    hw 15

--- 15-homework/index.js
+++ 15-homework/index.js
@@ -12,13 +12,25 @@ console.log("value: ", idGenerator.next().value);
 
 //Task 2
 
+function* newFontGenerator(value) {
+    let currentValue = 14;
+  while (currentValue) {
+    if (value === "up") {
+      yield currentValue = currentValue + 2;
+    } else if (value === "down") {
+      yield currentValue = currentValue - 2;
+    } else yield currentValue;
+  }
+}
 
+const fontGeneratorUp = newFontGenerator("up");
+const fontGeneratorDown = newFontGenerator("down");
 const fontGenerator = newFontGenerator(14);
-console.log('font up:', fontGenerator.next("up").value);
-console.log('font up:', fontGenerator.next("up").value);
-console.log('font up:', fontGenerator.next("up").value);
-console.log(fontGenerator.next().value);
-console.log('font down', fontGenerator.next("down").value);
-console.log('font down', fontGenerator.next("down").value);
-console.log('font down', fontGenerator.next("down").value);
-console.log(fontGenerator.next().value);
+console.log("font up:", fontGeneratorUp.next("up").value);
+console.log("font up:", fontGeneratorUp.next("up").value);
+console.log("font up:", fontGeneratorUp.next("up").value);
+console.log("current font:", fontGenerator.next().value);
+console.log("font down", fontGeneratorDown.next("down").value);
+console.log("font down", fontGeneratorDown.next("down").value);
+console.log("font down", fontGeneratorDown.next("down").value);
+console.log("current font:", fontGenerator.next().value);
